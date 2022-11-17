9a02e525e305bba9ee26c25efc3d04ac57f4cb85
GiacomoRattazzi <giacomo.rattazzi.1@unil.ch>
2022-11-17 19:53:45 +0100

    comment bean

---
+++ src/main/java/bookaroom/v2/beans/CommentBean.java
@@ -0,0 +1,52 @@
+package bookaroom.v2.beans;
+
+//import static bookaroom.v2.beans.UserController.findByUsername;
+import bookaroom.v2.exceptions.DoesNotExistException;
+import bookaroom.v2.models.User;
+import bookaroom.v2.models.Comment;
+import bookaroom.v2.database.MockDatabase;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+/*
+ * @author Team BookARoom
+ */
+
+@Named(value = "loginBean")
+@SessionScoped
+public class CommentBean implements Serializable{
+
+ 
+    private String comment = "";
+    private LocalDateTime now = LocalDateTime.now();
+    private DateTimeFormatter formatterComment = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            
+
+    public static ArrayList<Comment> getComments() {
+        return MockDatabase.getInstance().getComments();
+    }
+    
+    public String getComment() {
+        return comment;
+    }
+    
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    
+    public LocalDateTime getTodayDate() {
+        return now;
+    }
+    
+
+    public void addCommentFromUser() {
+        User user = LoginBean.getUserLoggedIn();
+        MockDatabase.getInstance().addAComment(new Comment(user.getUsername()+": "+comment+" ("+getTodayDate().format(formatterComment)+")"));
+        
+
+    }
+ 
+}
